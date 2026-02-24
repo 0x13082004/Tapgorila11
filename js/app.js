296405bb7aa4ac2447ff57b0786fc0953c0fd9d8
0x13082004 <0x13082004@gmail.com>
2026-02-24 19:33:19 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -71,7 +71,7 @@ const ACTION_TAP = "TAP";
 // IMPORTANT: Per Base Account docs, the `dataSuffix` capability must be an object
 // with a `value` field (hex string). Passing a raw string will throw:
 //   "Expected object, received string".
-const BUILDER_CODE = "bc_125griap";
+const BUILDER_CODE = "bc_fjhmk0s9";
 const builderCodeSuffix = Attribution.toDataSuffix({ codes: [BUILDER_CODE] });
 
 function isHexAddress(addr) {
